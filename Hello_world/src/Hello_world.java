50947a86958f2d4a52e91d1133c477d29b6a1921
handokone <18handokon72@millshighsch.org>
2016-09-13 08:40:59 -0700

    Hello_worldv1.0

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nicolas Handoko 
+ * @version 1.0
+ *
+ */
+public class Hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world! From Nicolas Handoko") ;
+		// Display the string
+	}
+
+}
